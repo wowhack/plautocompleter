eba68387f3b493271052952299ed1424fcaef5a6
Jens Ljungblad <jens.ljungblad@gmail.com>
2014-08-05 20:39:18 +0200

    Use intense scrubland to populate playlist

--- scripts/plautocompleter/playlist.js
+++ scripts/plautocompleter/playlist.js
@@ -17,13 +17,25 @@
   var fetch = function(playlistUri, onSuccess) {
     var result = {};
 
-    fetchFromSpotify(playlistUri, function(data){
+    fetchFromSpotify(playlistUri, function(data) {
       result.name = data.name;
-      result.tracks = $.map(data.tracks.items, function(obj) {
-        return obj.track;
+      result.tracks = $.map(data.tracks.items, function(item) {
+        return {
+          artist: item.track.artists[0].name,
+          name: item.track.name,
+          uri: item.track.uri
+        };
       });
 
-      onSuccess(result);
+      // Make some fetching from the intense scrubland
+      var ids = $.map(result.tracks.slice(0, 1), function(item) {
+        return item.uri;
+      });
+
+      fetchFromIntenseScrubland(ids, function(tracks) {
+        result.tracks = result.tracks.concat(tracks);
+        onSuccess(result);
+      });
     });
   };
 
@@ -49,12 +61,21 @@
     });
   };
 
-  // var generatePlaylistFromApi = function(playlist, onSuccess) {
-
-
-  //     onSuccess(tracks);
-  //   });
-  // };
+  var fetchFromIntenseScrubland = function(tracks, onSuccess) {
+    $.ajax({
+      url: 'http://intense-scrubland-2743.herokuapp.com/generate_playlist/' + tracks.join(',') + '/10',
+      success: function(response) {
+        response = JSON.parse(response);
+        onSuccess($.map(response.songs, function(item) {
+          return {
+            artist: item.artist_name,
+            name: item.title,
+            uri: item.id
+          };
+        }));
+      }
+    });
+  };
 
   /* Export public interface */
 
